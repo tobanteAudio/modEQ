ae30de8212fd5122a51efcd1311e0c3fb2448f8d
Tobias Hienzsch <post@tobias-hienzsch.de>
2018-09-05 18:57:59 +0200

    Format linux install script

--- install_linux.sh
+++ install_linux.sh
@@ -4,13 +4,13 @@ JUCE_PROJECT_NAME=modEQ
 BUILD_MODE=$1
 VST_DIRECTORY=~/.vst/tobanteAudio
 
-if [ "$BUILD_MODE" = "" ]
-  then BUILD_MODE=Release
+if [ "$BUILD_MODE" = "" ] then
+    BUILD_MODE=Release
 fi
 
 if ! [ -x "$(command -v Projucer)" ]; then
-  echo 'Error: Projucer is not installed.' >&2
-  exit 1
+    echo 'Error: Projucer is not installed.' >&2
+    exit 1
 fi
 
 echo "Resaving $JUCE_PROJECT_NAME.jucer project"
@@ -23,11 +23,11 @@ make CONFIG="$BUILD_MODE" -j$(nproc)
 
 
 if [ -d "$VST_DIRECTORY" ]; then
-  # Control will enter here if $DIRECTORY exists.
-  echo "$VST_DIRECTORY already created"
+    # Control will enter here if $DIRECTORY exists.
+    echo "$VST_DIRECTORY already created"
 else
-echo "Creating $VST_DIRECTORY"
-  mkdir $VST_DIRECTORY
+    echo "Creating $VST_DIRECTORY"
+    mkdir $VST_DIRECTORY
 fi
 
 
