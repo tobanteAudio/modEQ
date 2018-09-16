f2c97fea2fa3d902a168e6490da8e460997719b2
Tobias Hienzsch <post@tobias-hienzsch.de>
2018-09-16 03:29:03 +0200

    Fix typo in license header in src files

--- Source/modEQ_editor.cpp
+++ Source/modEQ_editor.cpp
@@ -1,15 +1,6 @@
-/*
-  ==============================================================================
-
-    This is the Frequalizer UI editor implementation
-
-  ==============================================================================
-*/
-
-//#include "Analyser.h"
 /* Copyright 2018 Tobias Hienzsch
  *
- * helm is free software: you can redistribute it and/or modify
+ * modEQ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
--- Source/processor/equalizer_processor.cpp
+++ Source/processor/equalizer_processor.cpp
@@ -1,6 +1,6 @@
 /* Copyright 2018 Tobias Hienzsch
  *
- * helm is free software: you can redistribute it and/or modify
+ * modEQ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
