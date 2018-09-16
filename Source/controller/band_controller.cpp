487ba5eef7cf3d698c19585e76df3c0bb20431a0
Tobias Hienzsch <post@tobias-hienzsch.de>
2018-09-16 03:24:40 +0200

    Add GPLv3 to source files

--- Source/controller/band_controller.cpp
+++ Source/controller/band_controller.cpp
@@ -1,14 +1,21 @@
-/*
-  ==============================================================================
-
-    BandController.cpp
-    Created: 2 Sep 2018 6:06:55pm
-    Author:  Tobias Hienzsch
-
-  ==============================================================================
-*/
+/* Copyright 2018 Tobias Hienzsch
+ *
+ * helm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * helm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with helm.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "band_controller.h"
+
 namespace TA
 {
 
--- Source/modEQ_editor.cpp
+++ Source/modEQ_editor.cpp
@@ -7,6 +7,22 @@
 */
 
 //#include "Analyser.h"
+/* Copyright 2018 Tobias Hienzsch
+ *
+ * helm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * helm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with helm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "modEQ_editor.h"
 #include "modEQ_processor.h"
 #include "view/social_buttons.h"
