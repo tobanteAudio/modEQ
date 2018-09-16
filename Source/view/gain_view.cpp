e08100e2d181bea8f16509b50e2119735403c459
Tobias Hienzsch <post@tobias-hienzsch.de>
2018-09-16 20:15:45 +0200

    Add gain view, controller & processor

---
+++ Source/view/gain_view.cpp
@@ -0,0 +1,56 @@
+/* Copyright 2018 Tobias Hienzsch
+ *
+ * modEQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * helm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with modEQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gain_view.h"
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+namespace TA
+{
+
+//==============================================================================
+GainView::GainView()
+{
+  // In your constructor, you should add any child components, and
+  // initialise any special settings that your component needs.
+}
+
+GainView::~GainView() {}
+
+void GainView::paint(Graphics& g)
+{
+  /* This demo code just fills the component's background and
+     draws some placeholder text to get you started.
+
+     You should replace everything in this method with your own
+     drawing code..
+  */
+
+  g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId)); // clear the background
+
+  g.setColour(Colours::grey);
+  g.drawRect(getLocalBounds(), 1); // draw an outline around the component
+
+  g.setColour(Colours::white);
+  g.setFont(14.0f);
+  g.drawText("GainView", getLocalBounds(), Justification::centred, true); // draw some placeholder text
+}
+
+void GainView::resized()
+{
+  // This method is where you should set the bounds of any child
+  // components that your component contains..
+}
+} // namespace TA
---
+++ Source/view/gain_view.h
@@ -0,0 +1,38 @@
+/* Copyright 2018 Tobias Hienzsch
+ *
+ * modEQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * helm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with modEQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+namespace TA
+{
+
+class GainView : public Component
+{
+public:
+  GainView();
+  ~GainView();
+
+  void paint(Graphics&) override;
+  void resized() override;
+
+private:
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainView)
+};
+
+} // namespace TA
