b6fc0106f91e740b55408cfe0fb4b649cd575e32
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-05-14 12:54:51 +0200

    Update version script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -26,7 +26,21 @@ NEW_VERSION="$1"
 echo "Current version: $CURRENT_VERSION"
 echo "New version: $NEW_VERSION"
 
-# sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/modEQ.jucer
-# sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/Doxyfile.in
-# sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/appveyor.yml
-# sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/test.txt
+# EXIT IF NEW & OLD VERSION MATCH
+if [ "$CURRENT_VERSION" = "$NEW_VERSION" ]; then
+    echo "Version was not incremented"
+    exit 0
+fi
+
+if git diff-index --quiet HEAD --; then
+    sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/modEQ.jucer
+    sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/Doxyfile.in
+    sed -i "s/$CURRENT_VERSION/$NEW_VERSION/" $ROOT_DIR/appveyor.yml
+else
+    echo "Commit changes before changing the version"
+    exit 0
+fi
+
+echo "Done"
+
+
