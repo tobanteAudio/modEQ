e789de3f7e6bc5f3a64ae0045d2bab753a499549
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 00:59:23 +0000

    Deploy tobanteAudio/modEQ to github.com/tobanteAudio/modEQ.git:gh-pages

--- search/functions_3.js
+++ search/functions_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['drawcombobox',['drawComboBox',['../classtobante_audio_1_1_tobante_look_and_feel.html#a59c3f9b18c49c9caf95dcde6364d0742',1,'tobanteAudio::TobanteLookAndFeel']]],
   ['drawfromsvg',['drawFromSVG',['../namespacetobante_audio.html#a919947ffbd1b720bf502757ee84e61cf',1,'tobanteAudio']]],
-  ['drawpopupmenubackground',['drawPopupMenuBackground',['../classtobante_audio_1_1_tobante_look_and_feel.html#a32662012f18e19972ac96a40812c9074',1,'tobanteAudio::TobanteLookAndFeel']]]
+  ['drawpopupmenubackground',['drawPopupMenuBackground',['../classtobante_audio_1_1_tobante_look_and_feel.html#a32662012f18e19972ac96a40812c9074',1,'tobanteAudio::TobanteLookAndFeel']]],
+  ['drawrotaryslider',['drawRotarySlider',['../classtobante_audio_1_1_tobante_look_and_feel.html#a6037a5a9b50f4daa17869227e11b434f',1,'tobanteAudio::TobanteLookAndFeel']]]
 ];
